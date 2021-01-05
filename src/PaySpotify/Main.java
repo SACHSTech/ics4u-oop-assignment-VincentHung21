7aab61b02a3e38e7e97a56bc694ee54aee0a6275
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2021-01-05 18:15:47 +0000

    Pulling Data from .txt and loading into Song[] array

--- src/PaySpotify/Main.java
+++ src/PaySpotify/Main.java
@@ -3,19 +3,55 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-    Song mylist = new Song(2,"Counting Stars","Orchestral",100,250, "May 15 2020");
-    System.out.println(mylist.getSongID());
+    //Song Object Array Creation Variables
+    Song[] AllSongs;
+    int intcounter = 0;
 
-    //File file = new file("Songs.txt");
-    BufferedReader reader = new BufferedReader(new FileReader("src/PaySpotify/Songs.txt"));
+    BufferedReader firstcount = new BufferedReader(new FileReader("src/PaySpotify/Songs.txt"));
     String line = null;
+    while ((line = firstcount.readLine()) != null) {
+      String[] values = line.split(",");
+    intcounter++;
+    }
+    firstcount.close();
+    BufferedReader reader = new BufferedReader(new FileReader("src/PaySpotify/Songs.txt"));
+    AllSongs = new Song[intcounter];
+
+
+    int intcount = 0;
+    int intx = 0;
+    int intSongID;
+    String strSongName;
+    String strGenre;
+    int intViews;
+    int intLength;
+    String strDate;
 
     while ((line = reader.readLine()) != null) {
       String[] values = line.split(",");
+      for(intcount = 0 ; intcount < 1 ; intcount++) {
+        intSongID = Integer.parseInt(values[intcount]);
+        intcount++;
+        strSongName = values[intcount];
+        intcount++;
+        strGenre = values[intcount];
+        intcount++;
+        intViews = Integer.parseInt(values[intcount]);
+        intcount++;
+        intLength = Integer.parseInt(values[intcount]);
+        intcount++;
+        strDate = values[intcount];
+        intcount++;
+        AllSongs[intx] = new Song(intSongID, strSongName, strGenre, intViews, intLength, strDate);
+        System.out.println("loaded");
+        intx++;
+      }
+      
       for (String str : values) {
-        System.out.println(str);
+       System.out.println(str);
       }
     }
     reader.close();
+    
   }
 }
