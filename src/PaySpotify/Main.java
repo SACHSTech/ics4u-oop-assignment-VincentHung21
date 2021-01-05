571c0fa4988753e4563e283c626ad46cf7938cc8
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2021-01-05 03:23:51 +0000

    Green Test getSongID

--- src/PaySpotify/Main.java
+++ src/PaySpotify/Main.java
@@ -1,7 +1,8 @@
 package PaySpotify;
 
-public class Main {
+public class Main {}
   public static void main(String[] args) {
     Song mylist = new Song(2,"Counting Stars","Orchestral",100,250, "May 15 2020");
+    System.out.println(mylist.getSongID());
   }
 }
